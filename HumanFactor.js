2497db71b378caac03e95f88bb4abde78311fb5c
sipubot <ddasik00@naver.com>
2016-11-03 14:19:30 +0900

    ..

---
+++ HumanFactor.js
@@ -0,0 +1,22 @@
+var HumanFactor = (function (HumanFactor,  undefined) {
+	"use strict";
+	//master function
+  var VALUE = {
+
+  };
+  var HUMAN = {
+
+  };
+  var FACTOR = {
+
+  };
+
+	HumanFactor.SetValue = function (setV) {
+	};
+  HumanFactor.SetHuman = function (setV) {
+	};
+  HumanFactor.ValueOf = function () {
+    return FACTOR;
+	};
+	return HumanFactor;
+})(window.HumanFactor || {});
