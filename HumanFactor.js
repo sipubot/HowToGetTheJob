e629a42a9d788d287866d7993546a192f835f839
sipubot <ddasik00@naver.com>
2016-11-03 14:55:21 +0900

    ..

--- HumanFactor.js
+++ HumanFactor.js
@@ -1,22 +1,119 @@
 var HumanFactor = (function (HumanFactor,  undefined) {
 	"use strict";
 	//master function
-  var VALUE = {
-
+  var GOAL = {
+    //sub total 100%
+    "Health": {
+      "Height": NaN,
+      "Weight": NaN,
+      "Insurance": NaN,
+    },
+    "Education": {
+      "Books": NaN,
+      "Seminar": NaN,
+      "Library": NaN,
+    },
+    "Money": {
+      "Salary": NaN,
+      "Bonus": NaN,
+      "Others": NaN,
+    },
+    "Honor": {
+      "Respect": NaN,
+      "Privacy": NaN,
+    },
+    "Time": {
+      "Hobby": NaN,
+      "Working": NaN,
+      "Vacation": NaN,
+      "Free": NaN,
+    },
+    "Relationship": {
+      "Friend": NaN,
+      "Coworker": NaN,
+      "Family": NaN,
+    },
+    "Surroundings": {
+      "Room": NaN,
+      "Food": NaN,
+      "Comunication": NaN,
+      "Entertainment": NaN,
+    },
   };
   var HUMAN = {
-
+    //max point 0~255
+    "Health": {
+      "Height": NaN,
+      "Weight": NaN,
+      "Insurance": NaN,
+    },
+    "Education": {
+      "Books": NaN,
+      "Seminar": NaN,
+      "Library": NaN,
+    },
+    "Money": {
+      "Salary": NaN,
+      "Bonus": NaN,
+      "Others": NaN,
+    },
+    "Honor": {
+      "Respect": NaN,
+      "Privacy": NaN,
+    },
+    "Time": {
+      "Hobby": NaN,
+      "Working": NaN,
+      "Vacation": NaN,
+      "Free": NaN,
+    },
+    "Relationship": {
+      "Friend": NaN,
+      "Coworker": NaN,
+      "Family": NaN,
+    },
+    "Surroundings": {
+      "Room": NaN,
+      "Food": NaN,
+      "Comunication": NaN,
+      "Entertainment": NaN,
+    },
   };
   var FACTOR = {
-
+    "MentalPoint" : NaN,
+    "HealthPoint" : NaN,
+    "Gold" : NaN,
+    "Level" : NaN,
+  };
+  var FACTORDETAIL = {
+    "Mental Point" : NaN,
   };
 
-	HumanFactor.SetValue = function (setV) {
+  function calcFACTOR() {
+
+  }
+
+
+	HumanFactor.SetGoal = function (category, subcategory, setV) {
+    GOAL[category][subcategory] = setV;
 	};
-  HumanFactor.SetHuman = function (setV) {
+  HumanFactor.SetGoal = function (category, subcategory, setV) {
+    HUMAN[category][subcategory] = setV;
+	};
+  HumanFactor.SetGoalALL = function (setV) {
+    GOAL = setV;
+	};
+  HumanFactor.SetHumanALL = function (setV) {
+    HUMAN = setV;
 	};
   HumanFactor.ValueOf = function () {
     return FACTOR;
+	};
+  HumanFactor.getGOAL = function () {
+    return GOAL;
+	};
+  HumanFactor.getHUMAN = function () {
+    return HUMAN;
 	};
 	return HumanFactor;
 })(window.HumanFactor || {});
